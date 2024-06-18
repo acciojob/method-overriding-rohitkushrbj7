e35a86a8490e2599410e15a2f3d09f098cbfb642
rohitkushrbj7 <rohitkush.rbj7@gmail.com>
2024-06-18 08:14:44 +0530

    overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+        System.out.println("Calling method 'meth' from class A: " + objB.meth());
+        System.out.println("Calling overridden method 'meth' from class B: " + objB.meth());
+    }
 }
